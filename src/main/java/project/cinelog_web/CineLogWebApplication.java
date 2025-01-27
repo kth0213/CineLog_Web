02373ac455fc1fe724dc6f39020726901b9ac292
kth0213 <asrai0412@google.com>
2025-01-27 18:00:13 +0900

    Initial commit

---
+++ src/main/java/project/cinelog_web/CineLogWebApplication.java
@@ -0,0 +1,13 @@
+package project.cinelog_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineLogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineLogWebApplication.class, args);
+    }
+
+}
