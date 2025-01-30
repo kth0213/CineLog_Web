4e55c56f2209b61025102b0325a6ffdc4cc098fe
kth0213 <asrai0412@gmail.com>
2025-01-30 22:43:31 +0900

    chore

--- src/main/java/project/cinelog_web/controller/MovieRatingController.java
+++ src/main/java/project/cinelog_web/controller/MovieRatingController.java
@@ -2,6 +2,7 @@ package project.cinelog_web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import project.cinelog_web.service.MovieRatingService;
 
 @RequiredArgsConstructor
