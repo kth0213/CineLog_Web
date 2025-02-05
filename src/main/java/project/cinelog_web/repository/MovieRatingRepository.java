0b2dcd838469f0078f763db06435c6fc1bea041d
kth0213 <asrai0412@gmail.com>
2025-02-05 20:33:18 +0900

    feat : MovieRating CRUD
    서비스 클래스에 Rating CRUD 구현

--- src/main/java/project/cinelog_web/repository/MovieRatingRepository.java
+++ src/main/java/project/cinelog_web/repository/MovieRatingRepository.java
@@ -3,7 +3,13 @@ package project.cinelog_web.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.cinelog_web.model.MovieRating;
 
+import java.util.List;
+
 public interface MovieRatingRepository extends JpaRepository<MovieRating, Long> {
     MovieRating findByMovieIdAndUserId(long movieId, long userId);
+
+    List<MovieRating> findByMovieId(long movieId);
+
+    List<MovieRating> findByUserId(long userId);
 
 }
--- src/main/java/project/cinelog_web/service/MovieRatingService.java
+++ src/main/java/project/cinelog_web/service/MovieRatingService.java
@@ -7,6 +7,7 @@ import project.cinelog_web.model.User;
 import project.cinelog_web.repository.MovieRatingRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class MovieRatingService {
@@ -17,18 +18,40 @@ public class MovieRatingService {
         this.movieRatingRepository = movieRatingRepository;
     }
 
-    public void create(Movie movie,Integer rating, User user, String comment) {
+    public void create(Movie movie, int rating, User user, String comment) {
         MovieRating m = new MovieRating();
         m.setMovie(movie);
         m.setRating(rating);
         m.setUser(user);
         m.setComment(comment);
         m.setCreateAt(LocalDate.now());
+        m.setUpdateAt(LocalDate.now());
         this.movieRatingRepository.save(m);
     }
 
-    public void delete(MovieRating movieRating) {
-        this.movieRatingRepository.delete(movieRating);
+    public List<MovieRating> getRatingByMovie(Long movieId) {
+        return movieRatingRepository.findByMovieId(movieId);
     }
 
+    public List<MovieRating> getRatingByUser(Long userId) {
+        return movieRatingRepository.findByUserId(userId);
+    }
+
+    public MovieRating updateRating(Long ratingId, int newRating, String newComment) {
+        MovieRating movieRating = this.movieRatingRepository.findById(ratingId)
+                .orElseThrow(()->new IllegalArgumentException("MovieRating not found"));
+
+        movieRating.setRating(newRating);
+        if(newComment != null) {
+            movieRating.setComment(newComment);
+        }
+        movieRating.setUpdateAt(LocalDate.now());
+        return this.movieRatingRepository.save(movieRating);
+    }
+
+    public void delete(Long ratingId) {
+        MovieRating rating = movieRatingRepository.findById(ratingId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 별점이 존재하지 않습니다."));
+        movieRatingRepository.delete(rating);
+    }
 }
