15b41b41e09d994943d9844b2b5b88a0784f08c1
kth0213 <asrai0412@gmail.com>
2025-02-07 22:29:08 +0900

    chore : 개발 환경 설정
    롬복 플러그인 오류 수정
    MovieRepository 오류 수정

--- src/main/java/project/cinelog_web/repository/MovieRepository.java
+++ src/main/java/project/cinelog_web/repository/MovieRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.cinelog_web.model.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    Movie findByName(String name);
+    Movie findByTitle(String name);
     Movie findById(long id);
 }
